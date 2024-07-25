66235862403c4d6e31e18269a30275b3a24cafaf
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-07-25 15:55:12 +0100

    Tailwind configuration for Font Faces work now

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
-import defaultTheme from "tailwindcss/defaultTheme";
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				'quicksand': ['Quicksand', ...defaultTheme.fontFamily.sans],
-				'exo2': ['"Exo 2"', ...defaultTheme.fontFamily.mono],
-			},
-		},
-	},
-	plugins: [],
-};
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand Variable', ...defaultTheme.fontFamily.sans],
+		mono: ['"Exo 2 Variable"', ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+}
