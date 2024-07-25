c4e98c9677856a9135f7416893e6fbb963bd3f5c
Marta <marta@MacBook-Pro-de-Marta-6.local>
2024-07-25 12:43:48 +0100

    Font-faces are not working in tailwind.config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,13 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			fontFamily: {
-				quicksand: ["Quicksand", "sans-serif"],
-				exo2: ["Exo2", "sans-serif"],
+				'quicksand': ['Quicksand', ...defaultTheme.fontFamily.sans],
+				'exo2': ['"Exo 2"', ...defaultTheme.fontFamily.mono],
 			},
 		},
 	},
