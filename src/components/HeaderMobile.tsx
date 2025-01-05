dbb73dbf1584914d07195305d5eb0774cbb8cc9d
ricard0g <ricardoguzman444@gmail.com>
2025-01-05 14:12:41 +0100

    Mobile Header shows links of the portfolio and also social media links on grid. We still have to figure out which bg-color to choose and also the icon that will go next to social media names

--- src/components/HeaderMobile.tsx
+++ src/components/HeaderMobile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function HeaderMobile() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -30,21 +30,123 @@ export default function HeaderMobile() {
             ></div>
             <div
               id="mid-bar"
-              className={`absolute top-[50%] w-full border-b-2 border-black transition-all ${isMenuOpen ? "translate-x-full opacity-0" : ""}`}
+              className={`absolute top-[50%] w-full border-b-2 border-black transition-all duration-300 ${isMenuOpen ? "translate-x-full opacity-0" : ""}`}
             ></div>
             <div
               id="bot-bar"
-              className={`absolute top-[80%] ml-[50%] w-1/2 border-b-2 border-black transition-all ${isMenuOpen ? "w-full rotate-45 -translate-y-3 -translate-x-[18px]" : ""}`}
+              className={`absolute top-[80%] ml-[50%] w-1/2 border-b-2 border-black transition-all duration-300 ${isMenuOpen ? "w-full rotate-45 -translate-y-3 -translate-x-[18px]" : ""}`}
             ></div>
           </button>
         </div>
       </section>
       <nav
-        className={`absolute top-0 w-screen h-screen z-[5] bg-shopify-light -translate-y-full transition-all ${isMenuOpen ? "translate-y-0" : ""}`}
+        className={`absolute top-0 w-screen h-screen z-[5] bg-shopify-light -translate-y-full transition-all duration-500 ${isMenuOpen ? "translate-y-0" : ""}`}
       >
-        <ul>
+        <ul className="relative p-0 top-1/4">
           <li>
-            <ul></ul>
+            <ul className="p-0 m-0">
+              <li
+                className={`mb-4 ${isMenuOpen ? "animate-fade-down animate-duration-200 animate-delay-500" : ""}`}
+              >
+                <a
+                  onClick={toggleMenu}
+                  href="#"
+                  className="inline-block w-full text-4xl text-center"
+                >
+                  Home
+                </a>
+              </li>
+              <li
+                className={`mb-4 ${isMenuOpen ? "animate-fade-down animate-duration-200 animate-delay-[700ms]" : ""}`}
+              >
+                <a
+                  onClick={toggleMenu}
+                  href="#skills"
+                  className="inline-block w-full text-4xl text-center"
+                >
+                  Skills
+                </a>
+              </li>
+              <li
+                className={`mb-4 ${isMenuOpen ? "animate-fade-down animate-duration-200 animate-delay-[900ms]" : ""}`}
+              >
+                <a
+                  onClick={toggleMenu}
+                  href="#About"
+                  className="inline-block w-full text-4xl text-center"
+                >
+                  About
+                </a>
+              </li>
+              <li
+                className={`mb-4 ${isMenuOpen ? "animate-fade-down animate-duration-200 animate-delay-[1100ms]" : ""}`}
+              >
+                <a
+                  onClick={toggleMenu}
+                  href="#projects"
+                  className="inline-block w-full text-4xl text-center"
+                >
+                  Projects
+                </a>
+              </li>
+              <li
+                className={`mb-4 ${isMenuOpen ? "animate-fade-down animate-duration-200 animate-delay-[1300ms]" : ""}`}
+              >
+                <a
+                  onClick={toggleMenu}
+                  href="#contact"
+                  className="inline-block w-full text-4xl text-center"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li
+            className={`w-full mt-24 ${isMenuOpen ? "animate-fade-up animate-duration-500 animate-delay-[1400ms]" : ""}`}
+          >
+            <ul className="grid grid-cols-2 grid-rows-2 place-items-start gap-x-5 gap-y-8 ">
+              <li className="px-10 ml-10 border-l-4 border-black">
+                <p className="text-lg">Regular updates on</p>
+                <a
+                  className="inline-block mt-2 text-3xl font-semibold"
+                  href="https://www.instagram.com"
+                  target="_blank"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li className="px-10 ml-10 border-l-4 border-black">
+                <p className="text-lg">Daily content on</p>
+                <a
+                  className="inline-block mt-2 text-3xl font-semibold"
+                  href="https://www.x.com"
+                  target="_blank"
+                >
+                  X
+                </a>
+              </li>
+              <li className="px-10 ml-10 border-l-4 border-black">
+                <p className="text-lg">See how I work on</p>
+                <a
+                  className="inline-block mt-2 text-3xl font-semibold"
+                  href="https://www.youtube.com"
+                  target="_blank"
+                >
+                  YouTube
+                </a>
+              </li>
+              <li className="px-10 ml-10 border-l-4 border-black">
+                <p className="text-lg">Let's connect on</p>
+                <a
+                  className="inline-block text-3xl mt-2 font-semibold"
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
