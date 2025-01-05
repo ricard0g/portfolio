e3160ffa4f889ce74525270c91f419a1b03d8608
ricard0g <ricardoguzman444@gmail.com>
2025-01-05 11:57:57 +0100

    Mobile Header Animation completed

--- src/components/HeaderMobile.tsx
+++ src/components/HeaderMobile.tsx
@@ -26,7 +26,7 @@ export default function HeaderMobile() {
           >
             <div
               id="top-bar"
-              className={`absolute top-[20%] mb-2 w-full border-b-2 border-black transition-all ${isMenuOpen ? "-rotate-45 top-1/2 left-1/2" : ""}`}
+              className={`absolute top-[20%] mb-2 w-full border-b-2 border-black transition-all duration-300 ${isMenuOpen ? "-rotate-45 translate-y-3" : ""}`}
             ></div>
             <div
               id="mid-bar"
@@ -34,7 +34,7 @@ export default function HeaderMobile() {
             ></div>
             <div
               id="bot-bar"
-              className={`absolute top-[80%] ml-[50%] w-1/2 border-b-2 border-black transition-all ${isMenuOpen ? "w-full rotate-45 top-1/2" : ""}`}
+              className={`absolute top-[80%] ml-[50%] w-1/2 border-b-2 border-black transition-all ${isMenuOpen ? "w-full rotate-45 -translate-y-3 -translate-x-[18px]" : ""}`}
             ></div>
           </button>
         </div>
