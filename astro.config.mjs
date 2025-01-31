9c3fcaf7047af1f7e81a3698b86a8b2d6efb7c28
ricard0g <ricardoguzman444@gmail.com>
2025-01-31 19:05:34 +0100

    Vercel Web Analytics added through vercel() adapter addition on astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,16 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ricardoguzman.dev",
   integrations: [react(), tailwind(), sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
+
